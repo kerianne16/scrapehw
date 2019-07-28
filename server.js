e712af119d1da7ea1ae09a21d7a508a0401db1b8
kerianne16 <kerianne116@gmail.com>
2019-07-28 11:49:50 -0400

    fixed errors

--- server.js
+++ server.js
@@ -1,42 +1,35 @@
-// Parses our HTML and helps us find elements
-const cheerio = require("cheerio");
-// Makes HTTP request for HTML page
-const axios = require("axios");
+// requiring all the packages we will be using
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 
-// First, tell the console what server.js is doing
-console.log("\n***********************************\n" +
-    "Grabbing every thread name and link\n" +
-    "from Into the Gloss:" +
-    "\n***********************************\n");
+const express = require("express");
+const app = express(); // start express
 
-// Making a request via axios for reddit's "webdev" board. The page's HTML is passed as the callback's third argument
-axios.get("https://intothegloss.com/").then(function (response) {
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultlayout: "main" }) //handlebars
+);
+app.set("view engine", "handlebars");
 
-    // Load the HTML into cheerio and save it to a variable
-    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-    var $ = cheerio.load(response.data);
+mongoose.connect("mongodb://localhost/scrapehw"); // mongo connection
+const db = mongoose.connection;
 
-    // An empty array to save the data that we'll scrape
-    var results = [];
-
-    // With cheerio, find each p-tag with the "title" class
-    // (i: iterator. element: the current element)
-    $("p.m-posts-item-intro").each(function (i, element) {
-
-        // Save the text of the element in a "title" variable
-        var title = $(element).text();
-
-        // In the currently selected element, look at its child elements (i.e., its a-tags),
-        // then save the values for any "href" attributes that the child elements may have
-        var link = $(element).children().attr("href");
-
-        // Save these results in an object that we'll push into the results array we defined earlier
-        results.push({
-            title: title,
-            link: link
-        });
-    });
-
-    // Log the results once you've looped through each of the elements found with cheerio
-    console.log(results);
+db.on("error", console.log.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Connected to Mongoose!")
 });
+
+const port = process.env.PORT || 3000; //setting up port
+app.listen(port, () => {
+    console.log(`Listening on PORT ${port}`);
+});
+
+app.use(logger("dev")); //using morgan
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+
